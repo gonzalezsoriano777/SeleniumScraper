568dc5590224420597fdfe1d03e2e6f6032e6ef0
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-06-03 19:28:49 -0500

    test

--- MyScraper/Program.cs
+++ MyScraper/Program.cs
@@ -15,34 +15,40 @@ namespace MyScraper
         static void Main(string[] args)
         {
 
-            
 
-            
+            #region Extra data will be deleted shortly
 
 
-                // Create a new instance of the Chrome driver.
-                // Note that it is wrapped in a using clause so that the browser is closed 
-                // and the webdriver is disposed (even in the face of exceptions).
+            // Create a new instance of the Chrome driver.
+            // Note that it is wrapped in a using clause so that the browser is closed 
+            // and the webdriver is disposed (even in the face of exceptions).
 
-                // Also note that the remainder of the code relies on the interface, 
-                // not the implementation.
+            // Also note that the remainder of the code relies on the interface, 
+            // not the implementation.
 
-                // Further note that other drivers (InternetExplorerDriver,
-                // ChromeDriver, etc.) will require further configuration 
-                // before this example will work. See the wiki pages for the
-                // individual drivers at http://code.google.com/p/selenium/wiki
-                // for further information.
-                using (IWebDriver driver = new ChromeDriver())
+            // Further note that other drivers (InternetExplorerDriver,
+            // ChromeDriver, etc.) will require further configuration 
+            // before this example will work. See the wiki pages for the
+            // individual drivers at http://code.google.com/p/selenium/wiki
+            // for further information.
+
+            #endregion
+
+
+
+            using (IWebDriver driver = new ChromeDriver())
                 {
                     //Notice navigation is slightly different than the Java version
                     //This is because 'get' is a keyword in C#
-                    driver.Navigate().GoToUrl("http://www.google.com/");
+
+                    
+                    driver.Navigate().GoToUrl("https://finance.yahoo.com/");
 
                     // Find the text input element by its name
                     IWebElement query = driver.FindElement(By.Name("q"));
 
                     // Enter something to search for
-                    query.SendKeys("CareerDevs");
+                    query.SendKeys("Apple");
 
                     // Now submit the form. WebDriver will find the form for us from the element
                     query.Submit();
