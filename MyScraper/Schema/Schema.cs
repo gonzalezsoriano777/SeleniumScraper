3bb3f0e7aadb037543062af968de1610b1671984
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-06-08 12:33:54 -0500

    Database implemented, now working on integrating the database

---
+++ MyScraper/Schema/Schema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyScraper
+{
+    private
+    private
+    private
+    private
+    private 
+    private
+
+    public class SchemaDB
+    {
+
+    }
+}
