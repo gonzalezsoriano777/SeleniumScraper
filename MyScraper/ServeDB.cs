9b81f326a77f73f8599ec1b1e88c2235b7df085e
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-06-08 11:51:26 -0500

    test

---
+++ MyScraper/ServeDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyScraper
+{
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ MyScraper/SqlDatabaseSetup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting;
+using NUnit.Framework;
+
+namespace MyScraper
+{
+    [TestFixture]
+    public class SqlDatabaseSetup
+    {
+
+        [Test()]
+        public static void InitializeAssembly(TestContext ctx)
+        {
+            // Setup the test database based on setting in the
+            // configuration file
+            SqlDatabaseTestClass.TestService.DeployDatabaseProject();
+            SqlDatabaseTestClass.TestService.GenerateData();
+        }
+
+    }
+}
