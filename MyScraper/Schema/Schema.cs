bcca04dea3d4e4b89cc0f6b7b7280aa2557ae611
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-06-10 19:03:34 -0500

    Created Schema outline for database connection.

--- MyScraper/Schema/Schema.cs
+++ MyScraper/Schema/Schema.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace MyScraper
 {
-    
-
-    public class SchemaDB
+  
+    public class StockDB
     {
+        private string symbol;
+        private double lastprice;
+        private double change;
+        private double chg;
+        private string currency;
+        private DateTime marketTime;
+        private double volume;
+        private double volumeAvg;
 
+
+        public StockDB()
+        {
+        }
+
+        public StockDB(string Symbol, double LastPrice, double Change, 
+            double Chg, string Currency, DateTime MarketTime, double Volume, double VolumeAvg)
+        {
+            this.symbol = Symbol;
+            this.lastprice = LastPrice;
+            this.change = Change;
+            this.chg = Chg;
+            this.currency = Currency;
+            this.marketTime = MarketTime;
+            this.volume = Volume;
+            this.volumeAvg = VolumeAvg;
+
+        }
     }
 }
