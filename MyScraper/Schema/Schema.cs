21f7c9486187332b73b42682e2793b8a3802d704
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-06-10 19:30:13 -0500

    test

--- MyScraper/Schema/Schema.cs
+++ MyScraper/Schema/Schema.cs
@@ -18,13 +18,18 @@ namespace MyScraper
         private double volume;
         private double volumeAvg;
 
-
-        public StockDB()
-        {
-        }
-
-        public StockDB(string Symbol, double LastPrice, double Change, 
-            double Chg, string Currency, DateTime MarketTime, double Volume, double VolumeAvg)
+        private string Symbol { get => symbol; set => symbol = value;}
+        private double LastPrice { get => lastprice; set => lastprice = value; }
+        private double Change { get => change; set => change = value; }
+        private double Chg { get => chg; set => chg = value; }
+        private string Currency { get => currency; set => currency = value; }
+        private DateTime MarketTime { get => marketTime; set => marketTime = value; }
+        private double Volume { get => volume; set => volume = value; }
+        private double VolumeAvg { get => volumeAvg; set => volumeAvg = value; }
+ 
+        // Constructor set for the columns of the table
+        public StockDB(string _symbol, double _lastPrice, double _change, 
+            double _chg, string _currency, DateTime _marketTime, double _volume, double _volumeAvg)
         {
             this.symbol = Symbol;
             this.lastprice = LastPrice;
